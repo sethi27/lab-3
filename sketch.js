a6349b66ed35ecf0c50994800bb6ca93038512d3
sethi27 <asethi@ocadu.ca>
2025-03-09 16:44:56 -0400

    c

--- sketch.js
+++ sketch.js
@@ -2,10 +2,8 @@ let brushSizeSlider;
 let colorPicker;
 let clearButton;
 let shapeSelect;
-let modeSelect;
 let currentShape = 'circle';
 let drawingColor;
-let drawingMode = 'normal';
 
 function setup() {
     let canvas = createCanvas(600, 400);
@@ -20,7 +18,7 @@ function setup() {
     createP('Color:').parent('controls');
     colorPicker = createColorPicker('#000000');
     colorPicker.parent('controls');
-    colorPicker.input(updateColor); 
+    colorPicker.input(updateColor);
     
     createP('Shape:').parent('controls');
     shapeSelect = createSelect();
@@ -29,14 +27,6 @@ function setup() {
     shapeSelect.option('square');
     shapeSelect.option('triangle');
     shapeSelect.changed(updateShape);
-    
-    createP('Drawing Mode:').parent('controls');
-    modeSelect = createSelect();
-    modeSelect.parent('controls');
-    modeSelect.option('normal');
-    modeSelect.option('eraser');
-    modeSelect.option('rainbow');
-    modeSelect.changed(updateMode);
     
     clearButton = createButton('Clear Canvas');
     clearButton.parent('controls');
@@ -54,17 +44,7 @@ function draw() {
 
 function drawShape(x, y) {
     let size = brushSizeSlider.value();
-    
-   
-    if (drawingMode === 'eraser') {
-        fill(255);
-        noStroke();
-    } else if (drawingMode === 'rainbow') {
-        fill(random(255), random(255), random(255));
-    } else {
-        fill(drawingColor);
-    }
-    
+    fill(drawingColor);
     noStroke();
     
     switch(currentShape) {
@@ -93,10 +73,6 @@ function updateShape() {
     currentShape = shapeSelect.value();
 }
 
-function updateMode() {
-    drawingMode = modeSelect.value();
-}
-
 function clearCanvas() {
     background(255);
 }
@@ -113,4 +89,4 @@ function keyPressed() {
     if (key === 'c' || key === 'C') {
         clearCanvas();
     }
-} 
+}
